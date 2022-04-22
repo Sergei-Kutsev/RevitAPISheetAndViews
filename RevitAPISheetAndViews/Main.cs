291f3e63fb5d319008fe042ac974960e7e6b1171
Сергей Куцев <sergeikutsev@yandex.ru>
2022-04-22 20:10:33 +0300

    Добавьте файлы проекта.

---
+++ RevitAPISheetAndViews/Main.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitAPISheetAndViews
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
